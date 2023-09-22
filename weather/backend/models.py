3d9b3237ee8ec77de0ee5e53367a1b8a3046a83e
BeDSmonster <yonghyeon1130@gmail.com>
2023-09-22 05:07:22 -0400

    problem1 - data modeling

--- weather/backend/models.py
+++ weather/backend/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+# Craete models
+class Weather(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    station_id = models.CharField(max_length=15)
+    date = models.DateField(null=False, blank=False)
+    temp_max = models.IntegerField(default=-999)
+    temp_min = models.IntegerField(default=-999)
+    precipitation = models.IntegerField(default=-999)
+
+    class Meta:
+        db_table = "weather"
+        unique_together = (("station_id", "date"),)
