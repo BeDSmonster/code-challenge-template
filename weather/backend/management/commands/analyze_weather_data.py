eb4655d63fb8e9c0b6f9ff1a8c3158de6f724e89
BeDSmonster <yonghyeon1130@gmail.com>
2023-09-24 16:02:59 -0400

    problem 3(edit) - ignore null values from calculation

--- weather/backend/management/commands/analyze_weather_data.py
+++ weather/backend/management/commands/analyze_weather_data.py
@@ -25,21 +25,38 @@ class Command(BaseCommand):
             ## for every station_id and year, calculate mean of max temperature and min temperature
             ## and accumulated precipitation
             station_stat_df = (
-                df.groupby(["year"])
-                .agg({"temp_max": "mean", "temp_min": "mean", "precipitation": "sum"})
-                .reset_index()
-            )
-
-        # looping though each row of statistics dataframe
-        for idx, row in station_stat_df.iterrows():
-            stat_data.append(
-                Stats(
-                    station_id=station_id,
-                    year=int(row["year"]),
-                    temp_max_avg=int(row["temp_max"]),
-                    temp_min_avg=int(row["temp_min"]),
-                    precipitation_accum=int(row["precipitation"]),
+                df.drop(
+                    # drop null value from temp_max column before calculation
+                    df[df["temp_max"] == -9999].index
                 )
-            )
+                .groupby(["year", "station_id"])
+                .agg({"temp_max": "mean"})
+                .reset_index()
+                .merge(
+                    df.drop(df[df["temp_min"] == -9999].index)
+                    .groupby(["year", "station_id"])
+                    .agg({"temp_min": "mean"})
+                    .reset_index(),
+                    how="outer",
+                )
+                .merge(
+                    df.drop(df[df["precipitation"] == -9999].index)
+                    .groupby(["year", "station_id"])
+                    .agg({"precipitation": "sum"})
+                    .reset_index(),
+                    how="outer",
+                )
+            ).fillna(-9999)
+            # looping though each row of statistics dataframe
+            for idx, row in station_stat_df.iterrows():
+                stat_data.append(
+                    Stats(
+                        station_id=station_id,
+                        year=int(row["year"]),
+                        temp_max_avg=int(row["temp_max"]),
+                        temp_min_avg=int(row["temp_min"]),
+                        precipitation_accum=int(row["precipitation"]),
+                    )
+                )
         # load into the database
-        Stats.objects.bulk_create(stat_data)
+        Stats.objects.bulk_create(stat_data, ignore_conflicts=True)
